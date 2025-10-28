a6ce16ceecad38212b89d667bd8f21f4a88729d1
rismanitalst <rismanitalst@gmail.com>
2025-10-29 04:18:27 +0700

    add row & column layout

--- lib/main.dart
+++ lib/main.dart
@@ -28,17 +28,77 @@ class MyHome extends StatelessWidget {
         backgroundColor: Colors.blueAccent,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Container(
-          width: double.infinity,
-          height: 120,
-          color: Colors.blueAccent,
-          child: const Center(
-            child: Text(
-              "ini adalah padding widget",
-              style: TextStyle(color: Colors.white, fontSize: 18),
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //ini bagian baris
+            const Text(
+              "Row Horizontal (→)",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-          ),
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black26),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _buildBox("A"),
+                  _buildBox("B"),
+                  _buildBox("C"),
+                ],
+              ),
+            ),
+            const SizedBox(height: 40),
+
+            //ini bagian kolom
+            const Text(
+              "Column Vertical (↓)",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black26),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _buildBox("A"),
+                  const SizedBox(height: 10),
+                  _buildBox("B"),
+                  const SizedBox(height: 10),
+                  _buildBox("C"),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBox(String label) {
+    return Container(
+      width: 60,
+      height: 60,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.blueAccent,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Text(
+        label,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
         ),
       ),
     );
